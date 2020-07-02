500dc5dbcf6480d81b60e35d6518ad4da4d14167
kripi__ <mainkriiper@gmail.com>
2020-07-02 19:57:10 +0300

    Updates

---
+++ precache-manifest.52c51568b3b0365d82b4d485eed22949.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4828a007511aa2c0dff703b90f0eb3d",
+    "url": "/DiboWebsite/index.html"
+  },
+  {
+    "revision": "c9cec7d0a0ad531e7fdd",
+    "url": "/DiboWebsite/static/css/main.9fa7f090.chunk.css"
+  },
+  {
+    "revision": "c74f620eab7f4aab652c",
+    "url": "/DiboWebsite/static/js/2.c0c0d517.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/DiboWebsite/static/js/2.c0c0d517.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c9cec7d0a0ad531e7fdd",
+    "url": "/DiboWebsite/static/js/main.3deb7e81.chunk.js"
+  },
+  {
+    "revision": "09b6ae383a776ebf17da",
+    "url": "/DiboWebsite/static/js/runtime-main.70bb2bae.js"
+  }
+]);
