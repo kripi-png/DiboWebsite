2ae0682dd6afb8c039f3f629eeb13443f1dcf834
kripi__ <mainkriiper@gmail.com>
2020-07-02 04:07:08 +0300

    more or less the default react app

---
+++ src/navbar.js
@@ -0,0 +1,9 @@
+import React from 'react';
+export default class Navbar extends React.Component {
+  render() {
+    return <ul class="navbar">
+            <li></li>
+            <li></li>
+          </ul>;
+  }
+}
