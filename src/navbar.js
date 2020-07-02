991247154b6734c8d4571bdfb514bebaf8c4a5b1
kripi__ <mainkriiper@gmail.com>
2020-07-02 20:07:02 +0300

    added gh-pages

--- src/navbar.js
+++ src/navbar.js
@@ -6,12 +6,21 @@ export default class Navbar extends React.Component {
     return <ul className="navbar">
             <li className="navItem title"><a href="/#"><span>Dibo</span></a></li>
             <li className="navItem"><a href="/commands.html">Commands<span>&trade;</span></a></li>
-            <li className="navItem"><a href="https://discord.gg/vn3ejPR" target="_blank">Dibo's Home <span className="heart">&#60;3</span></a></li>
+            <li className="navItem">
+              <a
+                href="https://discord.gg/vn3ejPR"
+                target="_blank"
+                rel="noopener noreferrer">
+                Dibo's Home <span className="heart">&#60;3</span>
+              </a>
+            </li>
             <li className="navItem inviteButton">
               <a
                 href="https://discordapp.com/oauth2/authorize?client_id=536500891969388564&scope=bot&permissions=290794542"
                 target="_blank"
-              >Invite to Server</a>
+                rel="noopener noreferrer">
+                Invite to Server
+              </a>
             </li>
           </ul>;
   }
